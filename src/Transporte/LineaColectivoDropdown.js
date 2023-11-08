a472d288150d274ed2ef9b25b5bd64a302fe3055
1708pmg <gutierrezpm61@gmail.com>
2023-11-07 21:31:39 -0300

    error

--- src/Transporte/LineaColectivoDropdown.js
+++ src/Transporte/LineaColectivoDropdown.js
@@ -1,3 +1,94 @@
+/*import React, { useState } from 'react';
+import Select from 'react-select';
+
+const LineaColectivoDropdown = ({ lineas, onSelectLinea, onSelectRecorrido, onSelectDestino, onSelectAgencia }) => {
+  const [selectedRecorrido, setSelectedRecorrido] = useState('');
+  const [selectedDestino, setSelectedDestino] = useState('');
+  const [selectedAgencia, setSelectedAgencia] = useState('');
+
+  const handleChange = (selectedOption) => {
+    if (selectedOption) {
+      onSelectLinea(selectedOption.value);
+    } else {
+      onSelectLinea('');
+    }
+  };
+
+  const handleRecorridoChange = (selectedOption) => {
+    if (selectedOption) {
+      setSelectedRecorrido(selectedOption.value);
+      onSelectRecorrido(selectedOption.value);
+    } else {
+      setSelectedRecorrido('');
+      onSelectRecorrido('');
+    }
+  };
+
+  const handleDestinoChange = (selectedOption) => {
+    if (selectedOption) {
+      setSelectedDestino(selectedOption.value);
+      onSelectDestino(selectedOption.value);
+    } else {
+      setSelectedDestino('');
+      onSelectDestino('');
+    }
+  };
+
+  const handleAgenciaChange = (selectedOption) => {
+    if (selectedOption) {
+      setSelectedAgencia(selectedOption.value);
+      onSelectAgencia(selectedOption.value);
+    } else {
+      setSelectedAgencia('');
+      onSelectAgencia('');
+    }
+  };
+
+  const options = lineas.map((linea) => ({ value: linea, label: linea }));
+
+  return (
+    <div className="linea-colectivo-dropdown">
+      <label htmlFor="lineaColectivo">Seleccionar línea de colectivo: </label>
+      <Select
+        id="lineaColectivo"
+        options={options}
+        onChange={handleChange}
+        isMulti={false}
+        placeholder="Seleccionar"
+      />
+
+      <label htmlFor="recorrido">Seleccionar recorrido: </label>
+      <Select
+        id="recorrido"
+        options={options}
+        onChange={handleRecorridoChange}
+        isMulti={false}
+        value={selectedRecorrido ? { value: selectedRecorrido, label: selectedRecorrido } : null}
+      />
+
+      <label htmlFor="destino">Seleccionar destino: </label>
+      <Select
+        id="destino"
+        options={options}
+        onChange={handleDestinoChange}
+        isMulti={false}
+        value={selectedDestino ? { value: selectedDestino, label: selectedDestino } : null}
+      />
+
+      <label htmlFor="agencia">Seleccionar agencia: </label>
+      <Select
+        id="agencia"
+        options={options}
+        onChange={handleAgenciaChange}
+        isMulti={false}
+        value={selectedAgencia ? { value: selectedAgencia, label: selectedAgencia } : null}
+      />
+    </div>
+  );
+};
+
+export default LineaColectivoDropdown;*/
+
 import React from 'react';
 
 const LineaColectivoDropdown = ({ lineas, onSelectLinea, lineasFiltradas }) => {
@@ -38,3 +129,68 @@ const LineaColectivoDropdown = ({ lineas, onSelectLinea, lineasFiltradas }) => {
 };
 
 export default LineaColectivoDropdown;
+
+
+
+/*import React, { useState } from 'react';
+
+const LineaColectivoDropdown = ({ lineas, onSelectLinea, onSelectRecorrido, onSelectDestino, onSelectAgencia }) => {
+  const [selectedRecorrido, setSelectedRecorrido] = useState('');
+  const [selectedDestino, setSelectedDestino] = useState('');
+  const [selectedAgencia, setSelectedAgencia] = useState('');
+
+  const handleChange = (event) => {
+    const selectedLinea = event.target.value;
+    onSelectLinea(selectedLinea);
+  };
+
+  const handleRecorridoChange = (event) => {
+    const recorrido = event.target.value;
+    setSelectedRecorrido(recorrido);
+    onSelectRecorrido(recorrido);
+  };
+
+  const handleDestinoChange = (event) => {
+    const destino = event.target.value;
+    setSelectedDestino(destino);
+    onSelectDestino(destino);
+  };
+
+  const handleAgenciaChange = (event) => {
+    const agencia = event.target.value;
+    setSelectedAgencia(agencia);
+    onSelectAgencia(agencia);
+  };
+
+  return (
+    <div className="linea-colectivo-dropdown">
+      <label htmlFor="lineaColectivo">Seleccionar línea de colectivo: </label>
+      <select id="lineaColectivo" onChange={handleChange}>
+        <option value="">Seleccionar</option>
+        {lineas.map((linea) => (
+          <option key={linea} value={linea}>
+            {linea}
+          </option>
+        ))}
+      </select>
+
+    
+      <label htmlFor="recorrido">Recorrido: </label>
+      <input type="text" id="recorrido" value={selectedRecorrido} onChange={handleRecorridoChange} />
+
+   
+      <label htmlFor="destino">Destino: </label>
+      <input type="text" id="destino" value={selectedDestino} onChange={handleDestinoChange} />
+
+     
+      <label htmlFor="agencia">Agencia: </label>
+      <input type="text" id="agencia" value={selectedAgencia} onChange={handleAgenciaChange} />
+    </div>
+  );
+};
+
+export default LineaColectivoDropdown;*/
+
+
+
+
--- src/Transporte/TransporteApi.jsx
+++ src/Transporte/TransporteApi.jsx
@@ -1,3 +1,179 @@
+/*import React, { useState, useEffect } from 'react';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import L from 'leaflet';
+import Select from 'react-select';
+import LineasImagenes from './LineasImagenes';
+import 'react-select/dist/react-select.css';
+
+// Define las líneas de colectivo filtradas
+const lineasFiltradas = ['5A', '7B', '19A', '24A', '29A', '92A', '109A', '124A', '132A', '148A'];
+
+// Define el componente principal
+const TransporteApi = () => {
+  // Define estados para el colectivo seleccionado, datos de transporte, carga y datos de líneas seleccionadas
+  const [selectedLinea, setSelectedLinea] = useState('');
+  const [selectedRecorrido, setSelectedRecorrido] = useState('');
+  const [selectedDestino, setSelectedDestino] = useState('');
+  const [selectedAgencia, setSelectedAgencia] = useState('');
+  const [transportData, setTransportData] = useState(null);
+  const [loadingTransport, setLoadingTransport] = useState(true);
+  const [selectedLineasData, setSelectedLineasData] = useState([]);
+
+  // Función para obtener datos de transporte desde la API
+  const fetchTransportData = (linea, recorrido, destino, agencia) => {
+    let apiUrl =
+      'https://apitransporte.buenosaires.gob.ar/colectivos/vehiclePositionsSimple?%20&client_id=cb6b18c84b3b484d98018a791577af52&client_secret=3e3DB105Fbf642Bf88d5eeB8783EE1E6';
+
+    if (linea) {
+      apiUrl += `&route_short_name=${linea}`;
+    }
+
+    fetch(apiUrl)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setTransportData(data);
+        setLoadingTransport(false);
+      })
+      .catch((ex) => {
+        console.error(ex);
+        setLoadingTransport(false);
+      });
+  };
+
+  // Efect para obtener datos de transporte al montar el componente y establecer un intervalo de actualización
+  useEffect(() => {
+    fetchTransportData();
+    const intervalId = setInterval(fetchTransportData, 31000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  // Efect para obtener datos de transporte al seleccionar una línea de colectivo
+  useEffect(() => {
+    fetchTransportData(selectedLinea, selectedRecorrido, selectedDestino, selectedAgencia);
+  }, [selectedLinea, selectedRecorrido, selectedDestino, selectedAgencia]);
+
+  // Efect para filtrar y actualizar los datos de líneas seleccionadas
+  useEffect(() => {
+    if (selectedLinea && transportData) {
+      const selectedData = transportData.filter((linea) => {
+        // Filtrar por número, recorrido, destino y agencia
+        return (
+          linea.route_short_name === selectedLinea &&
+          (!selectedRecorrido || linea.recorrido === selectedRecorrido) &&
+          (!selectedDestino || linea.trip_headsign === selectedDestino) &&
+          (!selectedAgencia || linea.agency_name === selectedAgencia)
+        );
+      });
+      setSelectedLineasData(selectedData);
+    }
+  }, [transportData, selectedLinea, selectedRecorrido, selectedDestino, selectedAgencia]);
+
+  // Define la posición inicial y la posición de la ciudad
+  const position = [-34.603851, -58.381775];
+  const cityPosition = position;
+
+  // Renderiza el componente
+  if (loadingTransport) {
+    return <h1>Cargando datos de tránsito</h1>;
+  }
+
+  const lineasOptions = lineasFiltradas.map((linea) => ({ value: linea, label: linea }));
+
+  return (
+    <div className="transporte-container">
+      <h4 className="ciudad-transporte">Transporte en Ciudad de Buenos Aires</h4>
+
+      
+      <div className="linea-colectivo-dropdown">
+        <label htmlFor="lineaColectivo">Seleccionar línea de colectivo: </label>
+        <Select
+          id="lineaColectivo"
+          options={lineasOptions}
+          onChange={(selectedOption) => setSelectedLinea(selectedOption.value)}
+          isMulti={false}
+          placeholder="Seleccionar"
+        />
+      </div>
+
+      <div className="linea-colectivo-dropdown">
+        <label htmlFor="recorrido">Seleccionar recorrido: </label>
+        <Select
+          id="recorrido"
+          options={lineasOptions}
+          onChange={(selectedOption) => setSelectedRecorrido(selectedOption.value)}
+          isMulti={false}
+          value={selectedRecorrido ? { value: selectedRecorrido, label: selectedRecorrido } : null}
+        />
+      </div>
+
+      <div className="linea-colectivo-dropdown">
+        <label htmlFor="destino">Seleccionar destino: </label>
+        <Select
+          id="destino"
+          options={lineasOptions}
+          onChange={(selectedOption) => setSelectedDestino(selectedOption.value)}
+          isMulti={false}
+          value={selectedDestino ? { value: selectedDestino, label: selectedDestino } : null}
+        />
+      </div>
+
+      <div className="linea-colectivo-dropdown">
+        <label htmlFor="agencia">Seleccionar agencia: </label>
+        <Select
+          id="agencia"
+          options={lineasOptions}
+          onChange={(selectedOption) => setSelectedAgencia(selectedOption.value)}
+          isMulti={false}
+          value={selectedAgencia ? { value: selectedAgencia, label: selectedAgencia } : null}
+        />
+      </div>
+
+   
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={cityPosition}>
+          <Popup>Ciudad de Buenos Aires.</Popup>
+        </Marker>
+
+       
+        {selectedLinea &&
+          selectedLineasData.map((linea, index) => (
+            <Marker
+              key={index}
+              position={[linea.latitude, linea.longitude]}
+              icon={
+                new L.Icon({
+                  iconRetinaUrl: LineasImagenes[linea.route_short_name],
+                  iconUrl: LineasImagenes[linea.route_short_name],
+                  popupAnchor: [-0, -0],
+                  iconSize: [32, 45],
+                })
+              }
+            >
+              <Popup>
+                <p>Línea: {linea.route_short_name}</p>
+                <p>Destino: {linea.trip_headsign}</p>
+                <p>Agencia: {linea.agency_name}</p>
+              </Popup>
+            </Marker>
+          ))}
+      </MapContainer>
+    </div>
+  );
+};
+
+export default TransporteApi;*/
+
+
+
+
+
+
 // Importa las bibliotecas y componentes necesarios de React y react-leaflet
 import React, { useState, useEffect } from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
@@ -7,7 +183,7 @@ import LineasImagenes from './LineasImagenes';
 
 // Define las líneas de colectivo filtradas
 const lineasFiltradas = ['5A', '7B', '19A', '24A', '29A', '92A', '109A', '124A', '132A', '148A'];
-/*const lineasFiltradas = ['5', '7', '19', '24', '29', '92', '109', '124', '132', '168'];*/
+
 
 // Define el componente principal
 const TransporteApi = () => {
@@ -22,9 +198,7 @@ const TransporteApi = () => {
     let apiUrl =
       'https://apitransporte.buenosaires.gob.ar/colectivos/vehiclePositionsSimple?%20&client_id=cb6b18c84b3b484d98018a791577af52&client_secret=3e3DB105Fbf642Bf88d5eeB8783EE1E6';
 
-    /*if (linea) {
-      apiUrl += `&route_short_name=${linea}`;
-    }*/
+  
 
     fetch(apiUrl)
       .then((resp) => resp.json())
@@ -73,14 +247,13 @@ const TransporteApi = () => {
     <div className="transporte-container">
       <h4 className="ciudad-transporte">Transporte en Ciudad de Buenos Aires</h4>
 
-      {/* Renderiza el componente LineaColectivoDropdown con las líneas filtradas */}
+     
       <LineaColectivoDropdown
         lineas={lineasFiltradas}
         onSelectLinea={setSelectedLinea}
         lineasFiltradas={lineasFiltradas}
       />
 
-      {/* Renderiza el contenedor del mapa con una capa de azulejos y un marcador para la ciudad */}
       <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -90,7 +263,6 @@ const TransporteApi = () => {
           <Popup>Ciudad de Buenos Aires.</Popup>
         </Marker>
 
-        {/* Renderiza los marcadores para las líneas seleccionadas con datos de imágenes */}
         {selectedLinea &&
           selectedLineasData.map((linea, index) => (
             <Marker
@@ -108,6 +280,7 @@ const TransporteApi = () => {
               <Popup>
                 <p>Línea: {linea.route_short_name}</p>
                 <p>Destino: {linea.trip_headsign}</p>
+                <p>Agencia: {linea.agency_name}</p>
               </Popup>
             </Marker>
           ))}
@@ -118,3 +291,134 @@ const TransporteApi = () => {
 
 export default TransporteApi;
 
+/*import React, { useState, useEffect } from 'react';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import L from 'leaflet';
+import LineaColectivoDropdown from './LineaColectivoDropdown';
+import LineasImagenes from './LineasImagenes';
+
+// Define las líneas de colectivo filtradas
+const lineasFiltradas = ['5A', '7B', '19A', '24A', '29A', '92A', '109A', '124A', '132A', '148A'];
+
+// Define el componente principal
+const TransporteApi = () => {
+  // Define estados para el colectivo seleccionado, datos de transporte, carga y datos de líneas seleccionadas
+  const [selectedLinea, setSelectedLinea] = useState('');
+  const [selectedRecorrido, setSelectedRecorrido] = useState('');
+  const [selectedDestino, setSelectedDestino] = useState('');
+  const [selectedAgencia, setSelectedAgencia] = useState('');
+  const [transportData, setTransportData] = useState(null);
+  const [loadingTransport, setLoadingTransport] = useState(true);
+  const [selectedLineasData, setSelectedLineasData] = useState([]);
+
+  // Función para obtener datos de transporte desde la API
+  const fetchTransportData = (linea, recorrido, destino, agencia) => {
+    let apiUrl =
+      'https://apitransporte.buenosaires.gob.ar/colectivos/vehiclePositionsSimple?%20&client_id=cb6b18c84b3b484d98018a791577af52&client_secret=3e3DB105Fbf642Bf88d5eeB8783EE1E6';
+
+    if (linea) {
+      apiUrl += `&route_short_name=${linea}`;
+    }
+
+    fetch(apiUrl)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setTransportData(data);
+        setLoadingTransport(false);
+      })
+      .catch((ex) => {
+        console.error(ex);
+        setLoadingTransport(false);
+      });
+  };
+
+  // Efect para obtener datos de transporte al montar el componente y establecer un intervalo de actualización
+  useEffect(() => {
+    fetchTransportData();
+    const intervalId = setInterval(fetchTransportData, 31000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  // Efect para obtener datos de transporte al seleccionar una línea de colectivo
+  useEffect(() => {
+    fetchTransportData(selectedLinea, selectedRecorrido, selectedDestino, selectedAgencia);
+  }, [selectedLinea, selectedRecorrido, selectedDestino, selectedAgencia]);
+
+  // Efect para filtrar y actualizar los datos de líneas seleccionadas
+  useEffect(() => {
+    if (selectedLinea && transportData) {
+      const selectedData = transportData.filter((linea) => {
+        // Filtrar por número, recorrido, destino y agencia
+        return (
+          linea.route_short_name === selectedLinea &&
+          (!selectedRecorrido || linea.recorrido === selectedRecorrido) &&
+          (!selectedDestino || linea.trip_headsign === selectedDestino) &&
+          (!selectedAgencia || linea.agency_name === selectedAgencia)
+        );
+      });
+      setSelectedLineasData(selectedData);
+    }
+  }, [transportData, selectedLinea, selectedRecorrido, selectedDestino, selectedAgencia]);
+
+  // Define la posición inicial y la posición de la ciudad
+  const position = [-34.603851, -58.381775];
+  const cityPosition = position;
+
+  // Renderiza el componente
+  if (loadingTransport) {
+    return <h1>Cargando datos de tránsito</h1>;
+  }
+
+  return (
+    <div className="transporte-container">
+      <h4 className="ciudad-transporte">Transporte en Ciudad de Buenos Aires</h4>
+
+    
+      <LineaColectivoDropdown
+        lineas={lineasFiltradas}
+        onSelectLinea={setSelectedLinea}
+        onSelectRecorrido={setSelectedRecorrido}
+        onSelectDestino={setSelectedDestino}
+        onSelectAgencia={setSelectedAgencia}
+      />
+
+     
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={cityPosition}>
+          <Popup>Ciudad de Buenos Aires.</Popup>
+        </Marker>
+
+   
+        {selectedLinea &&
+          selectedLineasData.map((linea, index) => (
+            <Marker
+              key={index}
+              position={[linea.latitude, linea.longitude]}
+              icon={
+                new L.Icon({
+                  iconRetinaUrl: LineasImagenes[linea.route_short_name],
+                  iconUrl: LineasImagenes[linea.route_short_name],
+                  popupAnchor: [-0, -0],
+                  iconSize: [32, 45],
+                })
+              }
+            >
+              <Popup>
+                <p>Línea: {linea.route_short_name}</p>
+                <p>Destino: {linea.trip_headsign}</p>
+                <p>Agencia: {linea.agency_name}</p>
+              </Popup>
+            </Marker>
+          ))}
+      </MapContainer>
+    </div>
+  );
+};
+
+export default TransporteApi;*/
